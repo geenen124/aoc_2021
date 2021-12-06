8e23e3a0cdb9b50dbb68387d86d2dbd806261659
Alex Geenen <ageenen@pm.me>
2021-12-06 20:24:26 +0100

    (day6): Fix Type Annotation parse

--- day6/parse.py
+++ day6/parse.py
@@ -2,7 +2,7 @@ import pathlib
 from typing import List
 
 
-def parse_lanternfish_input(input_data: pathlib.Path):
+def parse_lanternfish_input(input_data: pathlib.Path) -> List[int]:
     buckets: List[int] = [0] * 9
     with input_data.open("r", encoding="utf-8") as input_lines:
         for i, line in enumerate(input_lines):
